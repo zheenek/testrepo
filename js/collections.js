87277533de984fc8fe3a5694d3a8bb09317d325c
zheenek <38116896+zheenek@users.noreply.github.com>
2018-07-30 16:32:12 +0300

    Add files via upload

---
+++ js/collections.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {    
+    $('.collection').slick({
+        dots: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+    ]});
+});
